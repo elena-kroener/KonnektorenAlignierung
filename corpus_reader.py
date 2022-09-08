57d2bff9049eb48db2cfbd53ee0a73cfd59ace49
opallch <opalleunglch@mailbox.org>
2022-09-08 11:26:00 +0200

    add minidom example (xml reader)

--- corpus_reader.py
+++ corpus_reader.py
@@ -1,8 +1,8 @@
 # parallel corpus reader
 import os
+from xml.dom import minidom
 from lib.extract_connectors import CONNECTOR_DE, CONNECTOR_EN, CONNECTOR_IT
 from data.corpus import *
-
 
 def list_xml_files(corpus_root):
     """returns xml filenames (without prefix) in the given corpus root directory."""
@@ -21,5 +21,12 @@ if __name__ == "__main__":
         with open(os.path.join(corpus_root, 'de', filepath), 'r') as f_de, \
         open(os.path.join(corpus_root, 'en', filepath), 'r') as f_en, \
         open(os.path.join(corpus_root, 'it', filepath), 'r') as f_it:
+            
             ## (2) XML-Inhalt extrahieren (eine separate Methode?)
-            pass  
+            # DE Beispiel
+            xml_de = minidom.parse(f_de)
+            sents_de = xml_de.getElementsByTagName('edu')
+            print(sents_de[0].firstChild.nodeValue)    
+            break
+
+            # methode for 3-sent-tuple (type: named tuple?) for all 3 corpora? 
