57c35d861f9daef4a98dba0162761b6bd68537ce
Elena <elena-kroener@web.de>
2022-09-10 12:24:23 +0200

    First version of align_connectors

--- corpus_to_html.py
+++ corpus_to_html.py
@@ -11,23 +11,38 @@ def read_connector_list(txt_filepath):
     return result
 
 def extract_connectors(triple, connector_list):
-    """Extracts connectors with their index for one triple of sentences"""
-    connectors_in_triple = {key: list() for key in ['de', 'en', 'it']}
+    """Extracts connectors as dict with their index as key"""
+    connectors_in_triple = {key: dict() for key in ['de', 'en', 'it']}
     for lang, sent in triple._asdict().items():
         for i, token in enumerate(word_tokenize(sent)):
             if token.lower() in connector_list:
-                connectors_in_triple[lang] += [(token, i)]
+                connectors_in_triple[lang].update({i: token})
     return connectors_in_triple
 
 def allign_connectors(extracted_connectors):
-    pass
+    """
+    Allign connectors into a dict of the form {color: {de: {index: word, index: word} en: {index: word, index: word}}, it: {index: word, index: word}}
+    """
+    colors = ['red', 'blue', 'yellow', 'green', 'pink']
+    # if only one connector
+    if len(extracted_connectors['de']) == 1 \
+        and len(extracted_connectors['en']) == 1 \
+        and len(extracted_connectors['it']) == 1:
+        return {colors.pop(0): extracted_connectors}
+    else:
+        print(extracted_connectors)
+        for index, connector in extracted_connectors['de'].items():
+            col = colors.pop(0)
+            allign = {col: {'de': {index: connector}}}
+            if index in extracted_connectors['en'].keys():
+                allign.update({col: {'en': extracted_connectors['en'][index]}})
+        return allign
 
-def sent_to_html_str(sent, connector_list):
+def sent_to_html_str(sent, color, language, alligned_connectors):
     """Converts a sentence to a html-string."""
     html_elements = ['<p>']
-    for token in word_tokenize(sent):
-        # red color for the connector
-        if token.lower() in connector_list:
+    for i, token in enumerate(word_tokenize(sent)):
+        if i in alligned_connectors[color][language].keys():
             html_elements.append(f'<font color="red">{token} </font>')
         else:
             html_elements.append(f'{token} ')
@@ -37,17 +52,18 @@ def sent_to_html_str(sent, connector_list):
 
 def write_as_html(path_out, sent_triples, connector_list):
     """Converts all sentence triples to html-strings and write to the given path."""
-    triple_id = 1
     with open(path_out, mode='w', encoding='utf-8') as f_out:
-        for triple in sent_triples:
+        for triple_id, triple in enumerate(sent_triples):
             extracted_connectors = extract_connectors(triple, connector_list)
-            allign_connectors(extracted_connectors)
+            #print(extracted_connectors)
+            alligned_connectors = allign_connectors(extracted_connectors)
+            #print(alligned_connectors)
             f_out.write(f'<p>{triple_id}</p>\n')
-            f_out.write(sent_to_html_str(triple.de, connector_list))
-            f_out.write(sent_to_html_str(triple.en, connector_list))
-            f_out.write(sent_to_html_str(triple.it, connector_list))
-            f_out.write('\n')
-            triple_id += 1
+            langs = {0: 'de', 1: 'en', 2: 'it'}
+            for i, sent in enumerate(triple):
+                for color in alligned_connectors.keys():
+                    f_out.write(sent_to_html_str(sent, color, langs[i], alligned_connectors))
+                f_out.write('\n')
 
 
 if __name__ == '__main__':
